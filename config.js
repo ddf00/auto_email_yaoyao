a6a4accafb7ff1797f057de22cb5eda566d7f916
lifengfeng <lifengfeng@weimiao.cn>
2021-10-11 17:22:52 +0800

    跟改备注

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   pass: 'yayjvhbwmgegiiaj', // 发送者邮箱MTP协议密码
   to: '1077244097@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-  location: '101010100',  // 和风天气-成都武侯区的id
+  location: '101010100',  // 和风天气-北京市的id
   type: '1,3,9', // 和风天气-生活指数type
   tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
   startDay: '2021-08-27', // 在一起的日期
