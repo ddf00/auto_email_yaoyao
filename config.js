f439e5a4a4dcd75b83bf60e08534e4a84b08d4ca
lifengfeng <lifengfeng317@starsee.cn>
2022-11-15 09:47:00 +0800

    config

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ module.exports = {
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
   user: '1325395157@qq.com', // 发送者邮箱
   pass: 'iynmhphpxrekjejh', // 发送者邮箱MTP协议密码
-  // to: '1077244097@qq.com', // 发送到谁，填邮箱
-  to: '1325395157@qq.com', // 发送到谁，填邮箱
+  to: '1077244097@qq.com', // 发送到谁，填邮箱
   cc: '1325395157@qq.com', // 抄送
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
   location: '101010100',  // 和风天气-北京市的id
